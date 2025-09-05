f9244cd18776a2d77d2925ce4dfb377fb048a309
jatinbhagat <YOUR_EMAIL_USED_ON_GITHUB>
2025-09-05 16:14:26 +0530

    Sitemap fix

--- decipherworld/urls.py
+++ decipherworld/urls.py
@@ -4,6 +4,7 @@ from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
 from django.conf.urls.static import static
 from core.sitemaps import StaticViewSitemap
+from core.views_robots import robots_txt
 
 # Sitemap configuration
 sitemaps = {
@@ -13,7 +14,7 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt', include('django.contrib.staticfiles.urls')),
+    path('robots.txt', robots_txt, name='robots_txt'),
     path('', include('core.urls')),
 ]
 
